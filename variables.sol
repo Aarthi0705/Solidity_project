a397f5fffeea44232575e83b602cf00331c50be3
Aarthy <77524356+Aarthi0705@users.noreply.github.com>
2023-03-12 14:25:27 +0530

    Update variables.sol

--- variables.sol
+++ variables.sol
@@ -1,14 +1,15 @@
 //SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.4;
-contract ReadWrite{
-uint nu1;
-bool nu2;
-address nu3;
-uint nu4;
 
+contract ReadWrite{
+    uint nu1;
+    bool nu2;
+    address nu3;
+    uint nu4;
+
+//Read
 function get() public view returns(uint){
-    //Read
     return nu1;
 }
 function get1() public view returns(bool){
@@ -20,13 +21,12 @@ function get2() public view returns(address){
 function get3() public view returns(uint){
     return nu4;
 }
+//Write
 function set(uint _nu1 )public{
-    //Write
     nu1 = _nu1;
-
 }
 function set1(bool _nu2)public{
-nu2 = _nu2;
+    nu2 = _nu2;
 }
 function set2(address _nu3)public{
     nu3 = _nu3;
